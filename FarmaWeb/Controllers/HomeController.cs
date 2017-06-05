3a562e444dd0721117c502b66c77715a995313bb
djosuhe <djosuehernandez94@gmail.com>
2017-06-05 14:23:16 -0600

    Maqueta Web Farmacia
    Pequeño template de una pagina web para una farmacia

---
+++ FarmaWeb/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FarmaWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Title = "FarmaWeb";
+
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            ViewBag.Title = "Productos";
+
+            return View();
+        }
+    }
+}
